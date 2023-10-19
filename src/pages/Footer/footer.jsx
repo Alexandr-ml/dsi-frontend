084dc448d1f307f4ea71358dc04a1377afc80e16
kat7058 <katerinag17019@gmail.com>
2023-10-18 23:00:54 -0600

    footericons

--- src/pages/Footer/footer.jsx
+++ src/pages/Footer/footer.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faTwitter, faGoogle, faInstagram, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+
 
 export default function Footer() {
   const footerStyle = {
@@ -8,80 +11,95 @@ export default function Footer() {
 
   return (
     <footer className="text-center text-white" style={footerStyle}>
-      <div className="container-fluid pt-4" style={{width:100,bottom:0}}>
-        <section className="row">
-          <div className="col">
-            <a
-              className="btn btn-link btn-floating btn-lg text-dark m-1"
-              href="#!"
-              role="button"
-              data-mdb-ripple-color="dark"
-            >
-              <i className="fab fa-facebook-f"></i>
-            </a>
-          </div>
-          <div className="col">
-            <a
-              className="btn btn-link btn-floating btn-lg text-dark m-1"
-              href="#!"
-              role="button"
-              data-mdb-ripple-color="dark"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>
-          </div>
-          <div className="col">
-            <a
-              className="btn btn-link btn-floating btn-lg text-dark m-1"
-              href="#!"
-              role="button"
-              data-mdb-ripple-color="dark"
-            >
-              <i className="fab fa-google"></i>
-            </a>
-          </div>
-          <div className="col">
-            <a
-              className="btn btn-link btn-floating btn-lg text-dark m-1"
-              href="#!"
-              role="button"
-              data-mdb-ripple-color="dark"
-            >
-              <i className="fab fa-instagram"></i>
-            </a>
-          </div>
-          <div className="col">
-            <a
-              className="btn btn-link btn-floating btn-lg text-dark m-1"
-              href="#!"
-              role="button"
-              data-mdb-ripple-color="dark"
-            >
-              <i className="fab fa-linkedin"></i>
-            </a>
-          </div>
-          <div className="col">
-            <a
-              className="btn btn-link btn-floating btn-lg text-dark m-1"
-              href="#!"
-              role="button"
-              data-mdb-ripple-color="dark"
-            >
-              <i className="fab fa-github"></i>
-            </a>
-          </div>
+      <div className="container-fluid pt-4" style={{ width: 100, bottom: 0 }} >
+        <section className="row justify-content-center">
+          <table className="justify-content-center">
+            <tr>
+              <td>
+                <div className="col-auto">
+                  <a
+                    className="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                  >
+                    <FontAwesomeIcon icon={faFacebookF} />
+                  </a>
+                </div>
+              </td>
+              <td>
+                <div className="col-auto">
+                  <a
+                    className="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                  >
+                    <FontAwesomeIcon icon={faTwitter} />
+                  </a>
+                </div>
+              </td>
+              <td>
+                <div className="col-auto">
+                  <a
+                    className="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                  >
+                    <FontAwesomeIcon icon={faGoogle} />
+                  </a>
+                </div>
+              </td>
+              <td>
+                <div className="col-auto">
+                  <a
+                    className="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                  >
+                    <FontAwesomeIcon icon={faInstagram} />
+                  </a>
+                </div>
+              </td>
+              <td>
+                <div className="col-auto">
+                  <a
+                    className="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                  >
+                    <FontAwesomeIcon icon={faLinkedin} />
+                  </a>
+                </div>
+              </td>
+              <td>
+                <div className="col-auto">
+                  <a
+                    className="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                  >
+                    <FontAwesomeIcon icon={faGithub} />
+                  </a>
+                </div>
+              </td>
+            </tr>
+          </table>
         </section>
       </div>
-
-      <div
-        className="text-center text-white p-5"
-        style={{ backgroundColor: "#214A87" }}
-      >
+      <div className="text-center text-white p-5" style={{ backgroundColor: "#214A87" }}>
         © 2023 Copyright:
-        <a className="text-white" href="#!">
+        <a className="text-white" href="https://listatareasdeproyectoespecifico--fluffy-caramel-bdfa25.netlify.app/">
           SGDP.com
         </a>
       </div>
     </footer>
+
+
+
   );
-}
+};
